5ddad5fc11bbcc13ec0c7f42ccbfb35ab057f907
JustinRoderick <justinroderick258@gmail.com>
2025-09-25 11:33:38 -0400

    fix schema for singlestore

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,24 +1,35 @@
 import {
   int,
+  bigint,
   text,
   singlestoreTable,
   index,
 } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
-
 export const files = singlestoreTable(
   "files_table",
   {
-    id: int("id").primaryKey().autoincrement(),
-    name: text("name"),
-    url: text("url"),
-    parent: text("parent"),
-    size: int("size"),
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+    size: int("size").notNull(),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
+
+export const folders = singlestoreTable(
+  "folders_table",
+  {
+    id: bigint("id", { mode: "number", unsigned: true })
+      .primaryKey()
+      .autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
   },
   (t) => {
     return [index("parent_index").on(t.parent)];
