c9b9f442edac3fefe1f8f5c2335e143369d4c8d5
JustinRoderick <justinroderick258@gmail.com>
2025-09-25 10:34:59 -0400

    file schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,26 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+  int,
+  text,
+  singlestoreTable,
+  index,
+} from "drizzle-orm/singlestore-core";
 
 export const users = singlestoreTable("users_table", {
   id: int("id").primaryKey().autoincrement(),
   name: text("name"),
   age: int("age"),
 });
+
+export const files = singlestoreTable(
+  "files_table",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    name: text("name"),
+    url: text("url"),
+    parent: text("parent"),
+    size: int("size"),
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)];
+  },
+);
